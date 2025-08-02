9722b5f6d19a408a0347de6f36de837f70a8dac4
Hemagirishwar.k <145684826+hemagirishwar@users.noreply.github.com>
2025-08-02 21:41:12 +0530

    Add files via upload

---
+++ Animal Classification/animalClassificationWebPage.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from tensorflow.keras.applications.efficientnet import preprocess_input
+
+model = tf.keras.models.load_model("C:/Users/hemag/Downloads/animal_classifier_model.keras")
+
+class_names = ['antelope', 'badger', 'bat', 'bear', 'bee', 'beetle', 'bison', 'boar', 'butterfly', 'cat', 'caterpillar', 'chimpanzee', 'cockroach', 'cow', 'coyote', 'crab', 'crow', 'deer', 'dog', 'dolphin', 'donkey', 'dragonfly', 'duck', 'eagle', 'elephant', 'flamingo', 'fly', 'fox', 'goat', 'goldfish', 'goose', 'gorilla', 'grasshopper', 'hamster', 'hare', 'hedgehog', 'hippopotamus', 'hornbill', 'horse', 'hummingbird', 'hyena', 'jellyfish', 'kangaroo', 'koala', 'ladybugs', 'leopard', 'lion', 'lizard', 'lobster', 'mosquito', 'moth', 'mouse', 'octopus', 'okapi', 'orangutan', 'otter', 'owl', 'ox', 'oyster', 'panda', 'parrot', 'pelecaniformes', 'penguin', 'pig', 'pigeon', 'porcupine', 'possum', 'raccoon', 'rat', 'reindeer', 'rhinoceros', 'sandpiper', 'seahorse', 'seal', 'shark', 'sheep', 'snake', 'sparrow', 'squid', 'squirrel', 'starfish', 'swan', 'tiger', 'turkey', 'turtle', 'whale', 'wolf', 'wombat', 'woodpecker', 'zebra']
+
+st.title("Animal Classifier 🐾")
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file).resize((224, 224)).convert("RGB")
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img_array = np.array(image)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions)]
+    confidence = np.max(predictions)
+    if confidence < 0.3:
+        st.markdown(f"### Prediction: Unknown")
+    else:
+        st.markdown(f"### Prediction: `{predicted_class}`")
+        st.markdown(f"Confidence: `{confidence:.2f}`")
